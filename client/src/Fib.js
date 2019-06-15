919eb29448a044f897c9c4d1b92fc3c9e305cee2
Sangkwun <wer2774@gmail.com>
2019-06-15 23:28:07 +0900

    fix

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,7 +35,8 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    let result = this.state.seenIndexes.map(({ number }) => number).join(', ');
+    return result
   }
 
   renderValues() {
